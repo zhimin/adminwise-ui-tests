cc69b1cf96070d54870c87e5f2bb2c1f3af2cf32
unknown <Administrator@.(none)>
2011-12-30 20:55:28 +1000

    fixed not selecting browser

--- selenium-webdriver-cucumber/features/support/env.rb
+++ selenium-webdriver-cucumber/features/support/env.rb
@@ -13,7 +13,7 @@ Dir["#{File.dirname(__FILE__)}/../../pages/*_page.rb"].each { |file| load file }
 $BASE_URL = $TESTWISE_PROJECT_BASE_URL || ENV["ADMINWISE_URL"] || "http://adminwise.heroku.com"
 
 browser_type = $TESTWISE_BROWSER.downcase.to_sym rescue nil  # set by TestWise if running in TestWise
-browser_type = RUBY_PLATFORM =~ /mingw/ ? :ie : :firefox
+browser_type ||= (RUBY_PLATFORM =~ /mingw/ ? :ie : :firefox)
 browser = Selenium::WebDriver.for(browser_type) 
 World(Test::Unit::Assertions)
 
