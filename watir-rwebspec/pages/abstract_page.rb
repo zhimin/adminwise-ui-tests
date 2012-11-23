63d8406df7094e4da317777cdca202182c4476cd
Zhimin Zhan <zhimin@Doms-MacBook-Pro.local>
2012-11-23 18:18:23 +1000

    merge rwebspec-watir and rwebspec-selenium with RWebSpec 4

--- watir-rwebspec/pages/abstract_page.rb
+++
@@ -1,9 +0,0 @@
-require 'rwebspec'
-
-class AbstractPage < RWebSpec::AbstractWebPage
-
-  def initialize(browser, text="")
-    super(browser, text)
-  end
-
-end
--- watir-rwebspec/pages/member_borrow_history_page.rb
+++
@@ -1,9 +0,0 @@
-
-class MemberBorrowHistoryPage < AbstractPage
-
-  def initialize(browser)
-    super(browser, "Member - Borrowing History ") # <= 
-  end
-
-
-end
